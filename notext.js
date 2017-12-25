d178d93fd10c4e65510729833ba99bf57a2b67c4
Owen Burbank <owen.burbank@4cinsights.com>
2017-12-24 20:02:30 -0800

    no iframes

--- notext.js
+++ notext.js
@@ -1,5 +1,7 @@
 document.querySelectorAll('.expando-button').forEach(function(v,i,a) {v.click()});
 var x = document.evaluate('//body//*[not(descendant-or-self::*[@class="expando"])][not(ancestor::*[@class="expando"])]', document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
 for ( var i=0 ; i < x.snapshotLength; i++ ) {x.snapshotItem(i).remove()}
+var x = document.evaluate('//iframe', document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+for ( var i=0 ; i < x.snapshotLength; i++ ) {x.snapshotItem(i).remove()}
 var y = document.evaluate('//*[@class="expando"]/descendant-or-self::*', document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
 for ( var i=0 ; i < y.snapshotLength; i++ ) {y.snapshotItem(i).style="width: 100%; height: auto"}
